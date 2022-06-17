f74a0ec9ff52580803e599e0731622e52dcdc6ec
Rinat Valiullov <4rontender@gmail.com>
2022-06-17 16:28:52 +0500

    manage window's lifecycle

--- quick-start/main.js
+++ quick-start/main.js
@@ -11,4 +11,16 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
